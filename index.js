7981c90e36aa904b46533650a759d6b8fefb3cf2
mostafijurbabu <mostafijurbabu45@gmail.com>
2025-08-28 13:51:23 +0600

    added js function

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let coins = 100;
 let favCount = 0;
 
-// Call button logic
+// call button area
+
 document.querySelectorAll(".call-btn").forEach((btn) => {
   btn.addEventListener("click", function () {
     let card = btn.closest(".helpline-card");
@@ -31,33 +32,32 @@ document.querySelectorAll(".call-btn").forEach((btn) => {
       "p-2 border rounded-md shadow-sm text-xs mt-3 border-none font-semibold";
     entry.innerHTML = `
         <strong>${serviceName}</strong> - ${serviceNumber}
-        <div class="text-sm text-gray-500">${timeString}</div>
+        <div class="text-xs text-right">${timeString}</div>
       `;
     historyContainer.appendChild(entry);
   });
 });
 
-// Heart icon logic
+// heart button area
+
 document.querySelectorAll(".fa-heart").forEach((heart) => {
   heart.addEventListener("click", function () {
     if (heart.classList.contains("fa-regular")) {
-      // Mark as favorite
       favCount++;
       heart.classList.remove("fa-regular");
       heart.classList.add("fa-solid", "text-red-500");
     } else {
-      // Unmark as favorite
       favCount--;
       heart.classList.remove("fa-solid", "text-red-500");
       heart.classList.add("fa-regular");
     }
 
-    // Update navbar counter
     document.getElementById("heart-counter").innerText = favCount;
   });
 });
 
-// Clear Call History
+// clear button area
+
 document.getElementById("clear-history").addEventListener("click", () => {
   document.getElementById("history-container").innerHTML = "";
 });
@@ -82,7 +82,7 @@ function handleCopy(text) {
 }
 
 navbarCopyBtn.addEventListener("click", () => {
-  handleCopy("Some text from navbar!");
+  handleCopy("Some text from navbar");
 });
 
 const cardCopyButtons = document.querySelectorAll(".helpline-card .fa-copy");
